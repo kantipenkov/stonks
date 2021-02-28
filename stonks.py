293899fb698f9a6a1284225f6c546b5ea6d3d0cd
kantipenkov <bulvikk@gmail.ccom>
2021-02-28 22:27:43 +0300

    add more stats, and to_dataframe implementation. Add to do list

--- stonks.py
+++ stonks.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from yahoofinancials import YahooFinancials
 from pprint import pprint
 import pandas as pd
@@ -23,11 +24,11 @@ class StockData:
     _roa = None
     _interest_coverage = None
     _current_ratio = None
-    _roic = None
-    _wacc = None
+    # _roic = None
+    # _wacc = None
     _div_yield = None
     _div_payout_ratio = None
-    _div_growth = None
+    # _div_growth = None
     _ocf_per_share = None
     _debt_to_equity = None
     _debt_to_ebidta = None
@@ -39,7 +40,7 @@ class StockData:
 
 
     _desired_pe = None
-    _desired_proce = None
+    _desired_price = None
 
     _financials = None
 
@@ -62,8 +63,6 @@ class StockData:
         self._pe = summary['trailingPE']
         self._forward_pe = summary['forwardPE']
         self._ps = summary['priceToSalesTrailing12Months']
-        self._eps = summary['trailingEps']
-        self._forward_eps = summary['forwardEps']
         self._div_yield = summary['dividendYield'] * 100
         self._div_payout_ratio = summary['payoutRatio']
         self._beta = summary['beta']
@@ -75,27 +74,54 @@ class StockData:
         self._net_margin = key_stats['profitMargins']
         self._short_interest = key_stats['shortPercentOfFloat']
         self._pb = key_stats['priceToBook']
+        self._eps = key_stats['trailingEps']
+        self._forward_eps = key_stats['forwardEps']
 
         # calculate by self
-        self._roe
-        self._roa
-        self._current_ratio
-        self._interest_coverage
-        self._roic
-        self._wacc
-        self._div_growth
+
+        # self._roic
+        # self._wacc
+        # self._div_growth
 
 
 
 
     def to_dataframe(self):
-        pass
+        return pd.DataFrame(
+            {
+                "Market Cap"                   : self.market_cap,
+                "p/e"                          : self.pe,
+                "Forward p/e"                  : self.forward_pe,
+                "p/b"                          : self.pb,
+                "p/s"                          : self.ps,
+                "eps"                          : self.eps,
+                "Forward eps"                  : self.forward_eps,
+                "Net Margin"                   : self.net_margin,
+                "ROE"                          : self.roe,
+                "ROA"                          : self.roa,
+                "Interest Coverage"            : self.interest_coverage,
+                "Current Ratio"                : self.current_ratio,
+                "Div %"                        : self.div_yield,
+                "Payout Ratio"                 : self.div_payout_ratio,
+                "Operating Cash Flow per Share": self.ocf_per_share,
+                "Short interest"               : self.short_interest,
+                "Beta"                         : self.beta,
+                # add hystory of earnings, essetsd equity and calculated growth
+            },
+            index = [self.ticker]
+        )
 
     @property
     def pe(self):
         if not self._pe:
             self._pe = self.yahoo_obj.get_current_price() * self.yahoo_obj.get_num_shares_outstanding() / self._financials.earnings_ttm
         return self._pe
+    
+    @property
+    def forward_pe(self):
+        if not self._forward_pe:
+            self._get_data()
+        return self._forward_pe
 
     @property
     def market_cap(self):
@@ -125,16 +151,22 @@ class StockData:
         return self._eps
     
     @property
+    def forward_eps(self):
+        if not self._forward_eps:
+            self._get_data()
+        return self._forward_eps
+    
+    @property
     def net_margin(self):
         if not self._net_margin:
             self._get_data()
-        return self._net_margin
+        return self._net_margin * 100
 
     @property
     def div_yield(self):
         if not self._div_yield:
             self._div_yield = self.yahoo_obj.get_dividend_yield()
-        return self._div_yield
+        return self._div_yield * 100
 
     @property
     def div_payout_ratio(self):
@@ -145,26 +177,103 @@ class StockData:
     @property
     def roe(self):
         if not self._roe:
-            # update stock finansial to get required data
-            # get formula from gurufocus
-            self._roe = self._financials
-        return self._roe
+            self._roe = self._financials.earnings_ttm / self._financials.stockholders_equity_avg
+        return self._roe * 100
+    
+    @property
+    def roa(self):
+        if not self._roa:
+            self._roa = self._financials.earnings_ttm / self._financials.total_assets_avg
+        return self._roa * 100
+    
+    @property
+    def interest_coverage(self):
+        if not self._interest_coverage:
+            self._interest_coverage = -1 * self.yahoo_obj.get_operating_income() / self.yahoo_obj.get_interest_expense()
+        return self._interest_coverage
+
+    @property
+    def current_ratio(self):
+        if not self._current_ratio:
+            self._current_ratio = self._financials.current_assets / self._financials.current_liabilities
+        return self._current_ratio
+
+    @property
+    def ocf_per_share(self):
+        if not self._ocf_per_share:
+            self._ocf_per_share = self._financials.ttm_operating_cash_flow / self.yahoo_obj.get_num_shares_outstanding()
+        return self._ocf_per_share
+
+    @property
+    def short_interest(self):
+        if not self._short_interest:
+            self._get_data()
+        return self._short_interest * 100
+
+    @property
+    def beta(self):
+        if not self._beta:
+            self._get_data()
+        return self._beta
+
+
 
 
 class StockFinansials:
-    quarterly = None
-    yearly = None
+    earnings_quarterly = None
+    erarnings_yearly = None
 
     def __init__(self, ticker, yahoo_obj):
         self.ticker = ticker
         self.yahoo_obj = yahoo_obj
         earnings_data = self.yahoo_obj.get_stock_earnings_data()
-        self.quarterly = pd.DataFrame(earnings_data[self.ticker]['financialsData']['quarterly'])
-        self.yearly = pd.DataFrame(earnings_data[self.ticker]['financialsData']['yearly'])
+        self.earnings_quarterly = pd.DataFrame(earnings_data[self.ticker]['financialsData']['quarterly'])
+        self.erarnings_yearly = pd.DataFrame(earnings_data[self.ticker]['financialsData']['yearly'])
+        # self.balances = pd.DataFrame({datetime.strptime(list(x.keys())[0], '%Y-%m-%d'): x[list(x.keys())[0]] for x in self.yahoo_obj.get_financial_stmts('annual', 'balance')['balanceSheetHistory'][self.ticker]}) 
+        # self.balances = self.balances[self.balances.columns.sort_values()]
+        self.balances_y = self._get_financial_data_frame('annual', 'balance')
+        self.income_y = self._get_financial_data_frame('annual', 'income')
+        self.cash_y = self._get_financial_data_frame('annual', 'cash')
+        self.cash_q = self._get_financial_data_frame('quarterly', 'cash')
+
+    def _get_financial_data_frame(self, frequency, statement_type):
+        mapping = {
+            "balance": "balanceSheetHistory",
+            "income": "incomeStatementHistory",
+            "cash": "cashflowStatementHistory",
+        }
+        key = mapping[statement_type]
+        if frequency == 'quarterly':
+            key = key + 'Quarterly'
+        data = self.yahoo_obj.get_financial_stmts(frequency, statement_type)
+        df = pd.DataFrame({datetime.strptime(list(x.keys())[0], '%Y-%m-%d'): x[list(x.keys())[0]] for x in data[key][self.ticker]})
+        df = df[df.columns.sort_values()]
+        return df
+
 
     @property
     def earnings_ttm(self):
-        return self.quarterly.loc[:,  "earnings"].sum()
+        return self.earnings_quarterly.loc[:,  "earnings"].sum()
+
+    @property
+    def stockholders_equity_avg(self, num_of_periods: int=2):
+        return self.balances_y.loc['totalStockholderEquity', :].tail(num_of_periods).sum() / num_of_periods
+
+    @property
+    def total_assets_avg(self, num_of_periods: int=2):
+        return self.balances_y.loc['totalAssets', :].tail(num_of_periods).sum() / num_of_periods
+    
+    @property
+    def current_assets(self):
+        return self.balances_y.loc['totalCurrentAssets', self.balances_y.columns[-1]]
+    
+    @property
+    def current_liabilities(self):
+        return self.balances_y.loc['totalCurrentLiabilities', self.balances_y.columns[-1]]
+
+    @property
+    def ttm_operating_cash_flow(self):
+        return self.cash_q.loc['totalCashFromOperatingActivities', :].sum()
 
 
 
